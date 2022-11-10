803deedabfa4c98db13cbe2a02fad2e5deced0dc
Kanai Lomayani <k.mahenge@gmail.com>
2022-11-10 11:45:24 +0300

    Update: settings and routes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,11 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Application definition
 
 INSTALLED_APPS = [
-    'imagekit',
+    'dashboard',
+    'settings',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

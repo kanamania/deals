542ad0bee96db79677cbb1c3cb83a8836f331bc4
Kanai Lomayani <k.mahenge@gmail.com>
2022-12-02 14:48:07 +0300

    Docker: setup complete

--- .docker/init/01-create-user.sql
+++
@@ -1,8 +0,0 @@
---create users
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'develop';
-CREATE USER 'root'@'%' IDENTIFIED BY 'develop';
-CREATE USER 'develop'@'localhost' IDENTIFIED BY 'develop';
---grand appropriated rights
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-GRANT ALL PRIVILEGES ON *.* TO 'develop'@'%';
-
--- backend/settings/management/commands/init_admin.py
+++ backend/settings/management/commands/init_admin.py
@@ -1,13 +1,14 @@
-from decouple import config
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+import os
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        username = config('DJANGO_SUPERUSER_USERNAME')
-        email = config('DJANGO_SUPERUSER_EMAIL')
-        password = config('DJANGO_SUPERUSER_PASSWORD')
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
         if not User.objects.filter(username=username).exists():
             print('Creating account for %s (%s)' % (username, email))
